80b1e41c473a72d315f6d2234a73584239ba652f
Archik-K <ar.kryatova@yandex.ru>
2023-10-28 15:31:22 +0300

    modal

---
+++ index.js
@@ -0,0 +1,11 @@
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("modal");
+
+openModalBtn.addEventListener("click", () => {
+	modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+	modal.style.display = "none";
+});
