742845f4dbcdaea89ea2fe335b2a9d3aa2684529
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2023-10-28 17:13:04 +0300

    finish

--- index-settings.js
+++ index-settings.js
@@ -54,9 +54,20 @@ function thanks() {
 
     thank.classList.remove('hiddenSpan');
 }
+// function validateCreditCard(){
+//     let sixteen = /^\d{16}$/
+//     let validateInput  = document.querySelector('.credit').value
+//     console.log(validateInput)
+//     if (sixteen.test(validateInput) == false){
+//         alert("Please enter your 16 digit credit card numbers")};
+
+// }
+
+
 
 buttonCard.addEventListener('submit', function changeValues(event) {
     event.preventDefault();
+    // validateCreditCard();
     clearInputs(cardInputs);
     thanks();
 })
